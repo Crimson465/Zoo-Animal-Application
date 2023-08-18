916530679d0c65089d3adec305862a7e74796c1b
Crimson465 <conradpeters465@yahoo.com>
2023-08-18 18:28:48 -0400

    removing old client directory.

--- final-project/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <header>
-          <div className="footer"><p id="footer">My Zoo developed by Josh Mundy, Chelsea Villard, and Syeda Ahmed</p></div>
-      </header>
-    )
-  }
-  
-  export default Footer
